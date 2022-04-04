add031b27015c00f338e951152ae53f5d56f7b1f
Guillaume Bouyer <guillaume.bouyer@me.com>
2022-04-04 12:08:47 +0200

    Exercice 2

--- exercice_2/exercice_2.js
+++ exercice_2/exercice_2.js
@@ -10,15 +10,50 @@ const visiteurs = [
 ];
 
 /**
- * 1. Vérifier si le nom de famille "Meineck" existe dans le tableau. Afficher un message en conséquence.
+ * 1. Vérifier si le nom de famille "Meineck" existe dans le tableau. 
+ *    Afficher un message en conséquence.
  */
+
+// Solution #1
+for (let i = 0; i < visiteurs.length; i++) {
+  // console.log(visiteurs[i]);
+  for (let j = 0; j < visiteurs[i].length; j++) {
+    // console.log(visiteurs[i][j]);
+    if (visiteurs[i][j] === 'Meineck') {
+      console.log('Hey ho ! Salut toi !');
+      // Permet de stopper la boucle
+      // Ne fonctionne pas avec un .forEach()
+      break;
+    }
+    else {
+      console.log('Je ne te connais pas');
+    }
+  }
+}
+
+// Solution #2
+visiteurs.forEach(visiteur => {
+  // .includes() permet de vérifier si une valeur est présente
+  // dans le tableau
+  if (visiteur.includes("Meineck")) {
+    console.log('Hey ho ! Salut toi ! #2');
+  }
+});
 
 
 /**
  * 2. Trier le tableau par ordre alphabétique selon les prénoms.
  */
+visiteurs.sort();
+console.log(visiteurs);
 
 
 /**
- * 3. Créer un tableau contenant que les adresses e-mails en utilisant l'instruction map().
+ * 3. Créer un tableau contenant que les adresses e-mails 
+ *    en utilisant l'instruction map().
  */
+const emails = visiteurs.map(visiteur => {
+  return visiteur[2];
+});
+
+console.log(emails);
